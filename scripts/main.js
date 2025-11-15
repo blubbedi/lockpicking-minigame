84576d92a32962c78bbe919fd24718faabac9f91
blubbedi <manueloehlers@web.de>
2025-11-15 14:04:16 +0100

    Add files via upload

---
+++ scripts/main.js
@@ -0,0 +1,12 @@
+// scripts/main.js
+
+console.log("lockpicking-minigame | geladen");
+
+Hooks.once("init", () => {
+  console.log("lockpicking-minigame | init");
+});
+
+Hooks.once("ready", () => {
+  console.log("lockpicking-minigame | ready");
+  // TODO: Hier kommt später dein Lockpicking-Minispiel rein
+});
