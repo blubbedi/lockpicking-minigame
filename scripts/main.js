fd587f7d5edc9abba6f1f97cf396282b676f2ac2
blubbedi <manueloehlers@web.de>
2025-11-16 11:23:27 +0100

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -2,19 +2,33 @@ const MODULE_ID = "lockpicking-minigame";
 
 console.log(`${MODULE_ID} | main.js geladen (globaler Kontext)`);
 
+// Hier NICHT mehr auf game.user.id zugreifen, weil game.user noch null sein kann
 Hooks.once("init", () => {
-  console.log(`${MODULE_ID} | init hook auf User=${game.user.id}, isGM=${game.user.isGM}`);
+  console.log(`${MODULE_ID} | init hook aufgerufen, game.user ist:`, game.user);
 });
 
 Hooks.once("ready", () => {
-  console.log(`${MODULE_ID} | ready hook auf User=${game.user.id}, isGM=${game.user.isGM}`);
+  const u = game.user;
+  console.log(
+    `${MODULE_ID} | ready hook aufgerufen auf Client`,
+    {
+      userId: u?.id ?? null,
+      isGM: u?.isGM ?? null
+    }
+  );
 
   // Einfaches Test-API im game-Namespace
   game.lockpickingTest = {
     ping() {
-      ui.notifications.info(`LockpickingTest.ping() auf User=${game.user.id}`);
+      ui.notifications.info(
+        `LockpickingTest.ping() auf User=${game.user?.id ?? "unbekannt"}`
+      );
+      console.log(`${MODULE_ID} | ping() wurde aufgerufen`, game.user);
     }
   };
 
-  console.log(`${MODULE_ID} | game.lockpickingTest gesetzt:`, typeof game.lockpickingTest);
+  console.log(
+    `${MODULE_ID} | game.lockpickingTest gesetzt:`,
+    typeof game.lockpickingTest
+  );
 });
